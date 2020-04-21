78c205d248f9f56e5ebf6569331c51a519f9f94d
hakril <project@hakril.net>
2020-04-21 21:18:21 +0200

    Added some comment about unsure high-level API choice/possibility to discuss

--- samples/device/device_manager.py
+++ samples/device/device_manager.py
@@ -7,6 +7,14 @@ for devcls in manager.classes:
     print("- {0!r}".format(devcls))
     for device in devcls.devices:
         print(u"    - [{dev.name}] <{dev.description}> ({dev.device_object_name})".format(dev=device))
+        # Je sais pas trop quoi faire comme API sur ce truc
+        # Apparement on peut avoir plusieurs logical_configuration par type
+        #   - Je sais meme pas si c'est possible en vrai meme si <CM_Get_Next_Log_Conf> existe
+        # Idées:
+        #   * get_allocated_conf + get_boot_conf & co
+        #   * logical_configurations() qui retourne la liste complete
+        #   * Je sais pas trop
+        #
         devconf = device.get_first_logical_configuration(gdef.ALLOC_LOG_CONF)
         if devconf:
             # import pdb;pdb.set_trace()
