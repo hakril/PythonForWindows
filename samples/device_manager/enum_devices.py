176066d91708e9f382b84b7cd6eecb20928883a1
hakril <project@hakril.net>
2020-05-10 23:15:41 +0200

    Added option --attributes to enum_device, and updated the sample accordingly

--- samples/device_manager/enum_devices.py
+++ samples/device_manager/enum_devices.py
@@ -12,7 +12,7 @@ def class_generator(filter=None):
         yield cls
 
 
-def main(clsfilter, enumerate_devices, print_devinst_resources):
+def main(clsfilter, enumerate_devices, print_devinst_resources, attributes):
     for devcls in class_generator(clsfilter):
         print(devcls)
         if not enumerate_devices:
@@ -20,6 +20,14 @@ def main(clsfilter, enumerate_devices, print_devinst_resources):
         # Enumerate devices
         for devinst in devcls.devices:
             print("  * {0}".format(devinst))
+
+            # Print attributes
+            if attributes:
+                print("    Attributes:")
+                for attr in attributes:
+                    value = getattr(devinst, attr)
+                    print("    * {0}={1}".format(attr, value))
+
             if not print_devinst_resources:
                 continue
             # Device resources
@@ -28,6 +36,7 @@ def main(clsfilter, enumerate_devices, print_devinst_resources):
                 # No allocated configuration
                 # Check boot conf ?
                 continue
+
             for resource in devconf.resources:
                 print("    * {0}".format(resource))
 
@@ -37,12 +46,14 @@ if __name__ == "__main__":
     parser.add_argument("--class", dest="clsfilter", default=None, help="The classe to list: default all")
     parser.add_argument("--no-print-devices", action="store_true", help="Prevent the listing of devices in the matching classes")
     parser.add_argument("--print-resources", action="store_true", help="Print the resources allocated to the device instance")
+    parser.add_argument("--attributes", nargs="+", help="The list of attributes to print for each discovered device instance")
 
     args = parser.parse_args()
     print(args)
     main(args.clsfilter,
             enumerate_devices=not args.no_print_devices,
-            print_devinst_resources=args.print_resources)
+            print_devinst_resources=args.print_resources,
+            attributes=args.attributes)
 
 
 
