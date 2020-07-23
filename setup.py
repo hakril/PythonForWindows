2cd9c287aa613ad2465b79e2823cc5b075e75e64
hakril <project@hakril.net>
2020-07-23 09:38:03 +0200

    Updated setup.py with long description

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 ﻿# -*- coding: utf-8 -*-
 import sys
+import os.path
 from setuptools import setup
 
 PKG_NAME = "PythonForWindows"
-VERSION  = "0.6"
+VERSION  = "0.6.0"
+
+# Load long description from README.md
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name = PKG_NAME,
     version = VERSION,
     author = 'Hakril',
-    author_email = 'none',
+    author_email = 'pfw@hakril.net',
     description = 'A codebase aimed to make interaction with Windows and native execution easier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license = 'BSD',
     keywords = 'windows python',
     url = 'https://github.com/hakril/PythonForWindows',
