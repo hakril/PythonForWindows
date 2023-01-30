0f501ab46d033749ad1c2f6a77593c063904d7c1
clement rouault <clement.rouault@exatrack.com>
2023-01-30 13:12:18 +0100

    Service API are now full Wide / service object now use unicode strings

--- tests/test_service.py
+++ tests/test_service.py
@@ -12,16 +12,17 @@ def test_services_process():
 
 
 def test_service_appinfo():
-    appinfo = windows.system.services[b"Appinfo"]
+    appinfo = windows.system.services["Appinfo"]
     assert appinfo.status.type & gdef.SERVICE_WIN32_OWN_PROCESS
     # Check other fields
-    assert appinfo.name == b"Appinfo"
-    assert appinfo.description == b"Application Information"
+    assert appinfo.name == "Appinfo"
+    assert appinfo.description == "Application Information"
+    assert appinfo.security_descriptor
 
 
 def test_service_start():
-    faxservice = windows.system.services[b"Fax"]
+    faxservice = windows.system.services["Fax"]
     # Just start a random serivce with a string
     # Used to check string compat in py2/py3
-    faxservice.start(b"TEST STRING")
+    faxservice.start("TEST STRING")
 
