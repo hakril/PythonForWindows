1b5097050bcccf20406ef3897d8cb24912e35bb2
hakril <project@hakril.net>
2024-05-20 18:50:30 +0200

    Remove breakpoint in test_security_descriptor_from_unicode_file()

--- tests/test_security.py
+++ tests/test_security.py
@@ -34,7 +34,6 @@ def test_security_descriptor_from_binary(binsd):
 
 def test_security_descriptor_from_unicode_file(tmpdir):
     TARGET_FILENAME = u"내 한국은 최고의 한국.txt"
-    import pdb;pdb.set_trace()
     TARGET_PATH = os.path.join(tmpdir, TARGET_FILENAME)
     with open(TARGET_PATH, "w") as f:
         f.write("Hello Test")
