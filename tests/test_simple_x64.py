006dce70548a1f5a3ccbffab905b4985115b8b17
hakril <project@hakril.net>
2024-06-27 14:03:39 +0200

    Add tests for non-handled instruction format in simple x86/x64

--- tests/test_simple_x64.py
+++ tests/test_simple_x64.py
@@ -222,6 +222,7 @@ def test_assembler():
 
 
     CheckInstr(Push)('RAX')
+    CheckInstr(Push, must_fail=True)('EAX')
     assert len(Push("RAX").get_code()) == 1
     CheckInstr(Push)('R15')
     CheckInstr(Push)(0x42)
@@ -234,7 +235,9 @@ def test_assembler():
     assert len(Pop("RAX").get_code()) == 1
 
 
+    CheckInstr(Call, must_fail=True)('EAX')
     CheckInstr(Call)('RAX')
+    CheckInstr(Call)('R14')
     CheckInstr(Call)(mem('[RAX + RCX * 8]'))
     CheckInstr(Cpuid)()
     CheckInstr(Xchg)('RAX', 'RSP')
@@ -294,6 +297,15 @@ def test_assembler():
     CheckInstr(Mov, must_fail=True)('ECX', mem('[ECX + RCX]'))
     CheckInstr(Mov, must_fail=True)('ECX', mem('[RBX + ECX]'))
     CheckInstr(Add, must_fail=True)('RAX', 0xffffffff)
+
+
+    CheckInstr(Jmp, must_fail=True)('EAX')
+    CheckInstr(Jmp)('RAX')
+    CheckInstr(Jmp)('R12')
+    CheckInstr(Jmp)(mem('[RAX]'))
+    CheckInstr(Jmp)(mem('[RAX + 2]'))
+    CheckInstr(Jmp)(mem('[0x12345678]'))
+    CheckInstr(Jmp)(mem('[R15 + 0x12345678]'))
 
 
     # Test some prefix / REP
