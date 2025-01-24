1bc7818552b47670da9a61d671c5c1af7f1660e9
hakril <project@hakril.net>
2025-01-25 00:50:25 +0100

    Still playing with simple_arm64

--- tests/test_simple_arm64.py
+++ tests/test_simple_arm64.py
@@ -35,8 +35,22 @@ class CheckInstr(object):
         self.expected_result = expected_result
         self.must_fail = must_fail
         self.debug = debug
+        self.callargs = None
+
 
     def __call__(self, *args):
+        assert args is not None
+        self.callargs = args
+        return self
+
+    def __repr__(self):
+        if self.must_fail:
+            return "MustFail:{0}{1}".format(self.instr_to_test.__name__, self.callargs)
+        return "{0}{1}".format(self.instr_to_test.__name__, self.callargs)
+
+    def dotest(self):
+        assert self.callargs is not None
+        args = self.callargs
         try:
             if self.debug:
                 import pdb;pdb.set_trace()
@@ -70,6 +84,7 @@ class CheckInstr(object):
             raise AssertionError("Not all bytes have been used by the disassembler")
         self.compare_mnemo(capres)
         self.compare_args(args, capres)
+        return True
 
     def compare_mnemo(self, capres):
         expected = self.instr_to_test.__name__.lower()
@@ -80,9 +95,15 @@ class CheckInstr(object):
 
     def compare_args(self, args, capres):
         capres_op = list(capres.operands)
-        if len(args) != len(capres_op):
-            raise AssertionError("Expected {0} operands got {1}".format(len(args), len(capres_op)))
-        for op_args, cap_op in zip(args, capres_op):
+        # We may have != number of operand as shift are:
+        #    - arguments for simple_arm64
+        #    - atribute of immediat for capstone
+        if not len(capres_op) <= len(args):
+            raise AssertionError("Expected at most {0} operands got {1}".format(len(args), len(capres_op)))
+
+        opargit = iter(args) # allow manually using next() to get next simple_arm64 arg for shift compare
+        # capres_op must be first in zip (as its smaller) or last next(opargit) will be consommed by zip
+        for cap_op, op_args in zip(capres_op, opargit):
             if isinstance(op_args, str):  # Register
                 if cap_op.type != capstone.arm64.ARM64_OP_REG:
                     raise AssertionError("Expected args {0} operands got {1}".format(op_args, capres_op))
@@ -91,8 +112,38 @@ class CheckInstr(object):
             elif isinstance(op_args, int_types):
                 if (op_args != cap_op.imm) and not (self.immediat_accepted and self.immediat_accepted == cap_op.imm):
                     raise AssertionError("Expected Immediat <{0}> got {1}".format(op_args, cap_op.imm))
+                cap_shift = cap_op.shift
+                if not (cap_shift.type == cap_shift.value == 0):
+                    self.compare_shift(next(opargit), cap_shift)
             else:
                 raise ValueError("Unknow argument {0} of type {1}".format(op_args, type(op_args)))
+
+        # Check that no argument were unused in args
+        # As args + shift should perfectly match the capres_op
+        sentinel = object()
+        nextarg = next(opargit, sentinel)
+        if nextarg != sentinel:
+            # Ignore a leading LSL #0 shift, as it should be authorized but not displayed by disassembler
+            shift = Shift.parse(nextarg)
+            if not (shift.type == "LSL" and shift.value == 0):
+                raise ValueError("Non consomated argument: {0} (probable non-encoded shift)".format(nextarg))
+
+    SHIFT_TYPE_TO_CAPSTONE = {
+        "LSL": capstone.arm64.ARM64_SFT_LSL,
+        "LSR": capstone.arm64.ARM64_SFT_LSR,
+        "ASR": capstone.arm64.ARM64_SFT_ASR,
+        "ROR": capstone.arm64.ARM64_SFT_ROR,
+        # "MSL": apstone.arm64.ARM64_SFT_MSL # Not yet used in PFW
+    }
+
+    def compare_shift(self, shiftstr, cap_shift):
+        shift = Shift.parse(shiftstr)
+        if not self.SHIFT_TYPE_TO_CAPSTONE[shift.type] == cap_shift.type:
+            raise ValueError("Shift type mismatch: expected {0} got {1}".format(shift.type, cap_shift.type))
+        if not shift.value == cap_shift.value:
+            raise ValueError("Shift value mismatch: expected {0} got {1}".format(shift.value, cap_shift.value))
+        return True
+
 
 def test_shift_parsing():
     assert Shift.parse("LSL #0")
@@ -110,32 +161,36 @@ def test_shift_parsing():
     assert not Shift.parse("LSX ##1")
     assert not Shift.parse("LSX #")
 
-def test_assembler():
-    CheckInstr(Add)('W0', 'W0', 0)
-    CheckInstr(Add)('W1', 'W0', 0)
-    CheckInstr(Add)('W30', 'W12', 0)
-    CheckInstr(Add)('W0', 'W0', 1)
+@pytest.mark.parametrize("checkinstr", [
+    CheckInstr(Add)('W0', 'W0', 0),
+    CheckInstr(Add)('W1', 'W0', 0),
+    CheckInstr(Add)('W30', 'W12', 0),
+    CheckInstr(Add)('W0', 'W0', 1),
+    CheckInstr(Add)('X0', 'X0', 0),
+    CheckInstr(Add)('X30', 'X12', 0),
+    CheckInstr(Add)('X0', 'X0', 1),
+    CheckInstr(Add)('X11', 'X12', 0x123),
+    CheckInstr(Add)('X11', 'X12', 0x123, "LSL #0"),
+    CheckInstr(Add)('X11', 'X12', 0x123, "LSL #12"),
+    CheckInstr(Add, must_fail=True)('X11', 'W12', 0x123), # Bitness mismatch
+    CheckInstr(Add, must_fail=True)('BADREG', 'X12', 0),
+    CheckInstr(Add, must_fail=True)('X11', 'X12', 0x123, "LSL #1234"),
+    CheckInstr(Add, must_fail=True)('X11', 'X12', 0x12345678),
 
-    CheckInstr(Add)('X0', 'X0', 0)
-    CheckInstr(Add)('X30', 'X12', 0)
-    CheckInstr(Add)('X0', 'X0', 1)
-    CheckInstr(Add)('X11', 'X12', 0x123)
-    # CheckInstr(Add)('X11', 'X12', 0x123, "LSL #0")
-    CheckInstr(Add)('X11', 'X12', 0x123, "LSL #12")
+    CheckInstr(Movz)('X0', 0),
+    CheckInstr(Movz)('X0', 0, "LSL #32"),
+    CheckInstr(Movz)('X18', 0, "LSL #48"),
+    CheckInstr(Movz)('W18', 0, "LSL #16"),
+    CheckInstr(Movz, must_fail=True)('X0', 0, "LSL #12"), # Invalid LSL for MovWideImmediat
+    CheckInstr(Movz, must_fail=True)('W0', 0, "LSL #32"),
+    CheckInstr(Movz, must_fail=True)('X0', 0, "ROR #32"),
 
-    # Error test todo
-    # CheckInstr(Add)('X11', 'W12', 0x123)
-    with pytest.raises(ValueError):
-        CheckInstr(Add)('BADREG', 'X12', 0)
-    with pytest.raises(ValueError):
-        CheckInstr(Add)('X11', 'X12', 0x123, "LSL #1234")
+    CheckInstr(Movk)('X0', 0x1234, "LSL #32"),
+    CheckInstr(Movk)('X18', 0x5678, "LSL #48"),
 
-    with pytest.raises(ValueError):
-        # Immediat too big for encoding
-        CheckInstr(Add)('X11', 'X12', 0x12345678)
-
-    CheckInstr(Ret)("X0")
-    CheckInstr(Ret, expected_result="ret ")("X30")
-    CheckInstr(Ret)()
-    with pytest.raises(ValueError):
-        CheckInstr(Ret)("W0")
+    CheckInstr(Ret)("X0"),
+    CheckInstr(Ret, expected_result="ret ")("X30"),
+    CheckInstr(Ret)(),
+], ids=CheckInstr.__repr__)
+def test_instruction_assembling(checkinstr):
+    assert checkinstr.dotest()
