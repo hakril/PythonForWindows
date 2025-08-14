fdda80fad6805b7711a0d0d21aa47457fa52675f
hakril <project@hakril.net>
2025-08-14 10:35:58 +0200

    Update version number to 1.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os.path
 from setuptools import setup
 
 PKG_NAME = "PythonForWindows"
-VERSION  = "1.0.2"
+VERSION  = "1.0.4"
 
 # Load long description from README.md
 this_directory = os.path.abspath(os.path.dirname(__file__))
